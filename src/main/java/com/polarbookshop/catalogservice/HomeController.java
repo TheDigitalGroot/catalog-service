01da15786581972897bc643b6e6b433293123eb4
sanjuktaw <74349659+sanjuktaw@users.noreply.github.com>
2023-04-16 06:49:29 +0530

    sadas

--- src/main/java/com/polarbookshop/catalogservice/HomeController.java
+++ src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+
 	private final PolarProperties polarProperties;
 
 	public HomeController(PolarProperties polarProperties) {
